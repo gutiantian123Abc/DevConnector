cce60eaad8ff5f691efd6734a70319f6438147c2
gutiantian123Abc <william.guxiangtian@gmail.com>
2019-05-02 08:21:02 -0700

    MOngodb

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const keys = require('./keys');
+const db = keys.mongoURI;
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(db, { useNewUrlParser: true });
+        console.log("Mongodb connected!");
+    } catch(err){
+        console.log(err.message);
+        //Exit process with failure
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
