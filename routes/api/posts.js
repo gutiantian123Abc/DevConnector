828d54ad2514c025f18932e3a610cb09e27a54ca
gutiantian123Abc <william.guxiangtian@gmail.com>
2019-05-08 11:25:21 -0700

    Post like

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -97,6 +97,25 @@ router.delete('/:id', auth, async (req, res) => {
     }
 });
 
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        //check if the post has already been liked
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ msg: 'Post already liked' });
+        }
+        post.likes.unshift({ user: req.user.id });
+        await post.save();
+        return res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server Error');
+    }
+});
+
 
 
 
