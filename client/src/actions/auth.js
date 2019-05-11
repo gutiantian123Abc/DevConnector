542867609b1264503b2fd2bb1b6e79c2f3173dc3
gutiantian123Abc <william.guxiangtian@gmail.com>
2019-05-11 09:34:55 -0700

    user login

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from "./types";
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
+} from "./types";
 import { setAlert } from "./alert";
 import setAuthToken from '../utils/setAuthToken';
 
@@ -16,6 +23,7 @@ export const loadUser = () => async dispatch => {
             type: USER_LOADED,
             payload: res.data
         });
+
     } catch (err) {
         dispatch({
             type: AUTH_ERROR
@@ -36,11 +44,11 @@ export const register = ({ name, email, password }) => async dispatch => {
 
     try{
         const res = await axios.post('/api/users', body, config);
-
         dispatch({
             type: REGISTER_SUCCESS,
             payload: res.data
         });
+        dispatch(loadUser());
     } catch (err) {
         const errors = err.response.data.errors;
         if(errors) {
@@ -49,6 +57,38 @@ export const register = ({ name, email, password }) => async dispatch => {
 
         dispatch({
             type: REGISTER_FAIL
+        });
+    }
+};
+
+
+
+//Login User
+export const login = ( email, password ) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const body = JSON.stringify({ email, password});
+
+    try{
+        const res = await axios.post('/api/auth', body, config);
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+        dispatch(loadUser());
+    } catch (err) {
+        const errors = err.response.data.errors;
+        if(errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: LOGIN_FAIL
         });
     }
 };
