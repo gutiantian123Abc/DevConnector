c8b5e4d9672c6f560f45ec597fa6eb406e46f172
gutiantian123Abc <william.guxiangtian@gmail.com>
2019-05-31 11:38:38 -0700

    View profile by id

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 import setAuthToken from "./utils/setAuthToken";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import "./App.css";
@@ -40,6 +41,7 @@ const App = () => {
               <Route exact path="/login" component={Login} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
               <PrivateRoute
                   exact
                   path="/dashboard"
